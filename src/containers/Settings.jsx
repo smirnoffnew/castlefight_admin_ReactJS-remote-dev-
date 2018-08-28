ded1da8981f0875b016b3cabdac17b0d712323c3
Sergey Antonov <ant.sergey75@gmail.com>
2018-08-28 14:56:43 +0300

    add settings formComponent

--- src/containers/Settings.jsx
+++ src/containers/Settings.jsx
@@ -1,79 +1,28 @@
 import React, { Component } from 'react';
-import AddButton from "../components/addButton";
 import Loading from "../components/loading";
 import axios from "axios";
+import Helper from "../helper";
 
 class TableContainer extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.helper = new Helper();
         this.state = {
             isLoaded: false,
             entity: '',
-            tableComponentProps: {
-                data: []
-            }
+            formData: {
+                initialFarmCount: "",
+                farmIndexAndOutputs: [],
+                farmIndexAndCosts: [],
+                farmIndexAndPositions: []
+            },
+            oldFormData:{
+                initialFarmCount: "",
+                farmIndexAndOutputs: [],
+                farmIndexAndCosts: [],
+                farmIndexAndPositions: []
+            },
         };
-    }
-
-    removeRecord = (entity, id) => {
-        console.log('delete', entity, ' ', id);
-        axios.delete(`http://178.128.163.251:5555/v1/${entity}/${id}`, {})
-            .then(() => this.getData())
-            .catch(function (error) {
-                console.error(error);
-            });
-    };
-
-    getData = () => {
-        const slug = this.props.history.location.pathname.substr(1);
-        const trueSlug = 'commons/farm'
-        console.log('trueSlug', trueSlug)
-        axios.get('http://178.128.163.251:5555/v1/' + trueSlug)
-            .then(response => {
-                const { data } = response;
-                console.log('response', data)
-                this.setState(() => {
-                    let output = [], i = 0;
-                    for (let item in data) {
-                        let value = data[i]
-                        if (i == 'farmIndexAndPositions') {
-                            output[item] = []
-                            for (let val in value) {
-                                output[item].push(<div>{'' + value[val].x + ' ' + value[val].y}</div>)
-                            }
-                        } else if (typeof value === 'object') {
-                            output[item] = []
-                            for (let val in value) {
-                                output[item].push(<div>{value[val]}</div>)
-                            }
-                        } else {
-                            output.push(<div>{value}</div>)
-                        }
-                        i++
-                    }
-
-                    console.log('output', output)
-
-                    return {
-                        isLoaded: true,
-                        entity: slug,
-                        tableComponentProps: {
-                            data: output,
-                            columns: [
-                                'Farm Index And Costs',
-                                'Farm Index And Outputs',
-                                'Farm Index And Positions',
-                                'Initial Farm Count',
-                                'Edit',
-                                'Delete'
-                            ],
-                        }
-                    };
-                });
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
     }
 
     componentDidMount() {
@@ -84,17 +33,274 @@ class TableContainer extends Component {
         this.getData();
     }
 
+
+    getData = () => {
+        axios
+            .get('http://178.128.163.251:5555/v1/commons/farm/')
+            .then(response => {
+                this.setState((prevState) => {
+                    return {
+                        ...prevState,
+                        formData: {
+                            initialFarmCount: response.data.initialFarmCount,
+                            farmIndexAndOutputs : this.dataFormatterToDisplay( response.data.farmIndexAndOutputs ),
+                            farmIndexAndCosts : this.dataFormatterToDisplay( response.data.farmIndexAndCosts ),
+                            farmIndexAndPositions : this.dataFormatterToDisplay( response.data.farmIndexAndPositions ),
+                        },
+                        oldFormData: {
+                            initialFarmCount: response.data.initialFarmCount,
+                            farmIndexAndOutputs : this.dataFormatterToDisplay( response.data.farmIndexAndOutputs ),
+                            farmIndexAndCosts : this.dataFormatterToDisplay( response.data.farmIndexAndCosts ),
+                            farmIndexAndPositions : this.dataFormatterToDisplay( response.data.farmIndexAndPositions ),
+                        },
+                        isLoaded: true
+                    }
+                });
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    };
+
+    saveData = () => {
+        axios
+            .post(
+                'http://178.128.163.251:5555/v1/commons/farm/',
+                {
+                    initialFarmCount: 3,
+                    farmIndexAndOutputs: this.dataFormatterToSave(this.state.formData.farmIndexAndOutputs),
+                    farmIndexAndCosts: this.dataFormatterToSave(this.state.formData.farmIndexAndCosts),
+                    farmIndexAndPositions: this.dataFormatterToSave(this.state.formData.farmIndexAndPositions),
+                }
+            )
+            .then(() => this.getData());
+    };
+    
+    resetData = () => {
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                formData: Object.assign({}, prevState.oldFormData)
+            }
+        })
+    };
+
+
+    dataFormatterToDisplay = (objectData) => {
+        return Object.keys(objectData).map(key=>({
+            uniqueId: this.helper.makeId(),
+            nameInput: key,
+            valueInput: objectData[key]
+        }))
+    };
+
+    dataFormatterToSave = (arrayData) => {
+        let objectData = {};
+
+        arrayData.forEach(item=>{
+            if (typeof item.valueInput === 'object') {
+                objectData[item.nameInput] = {
+                    x: Number(item.valueInput.x),
+                    y: Number(item.valueInput.y),
+                }
+            } else {
+                objectData[item.nameInput] = Number(item.valueInput);
+            }
+        });
+        return objectData;
+    };
+
+
+    addInput = (arrayName, isValues) => {
+        let newDataObject = {
+            nameInput: Number(this.state.formData[arrayName][this.state.formData[arrayName].length - 1].nameInput) + 1,
+            uniqueId: this.helper.makeId(),
+        };
+        isValues ? newDataObject.valueInput = {x: 0, y: 0} : newDataObject.valueInput = 0;
+
+        this.setState((prevState) => {
+            let arrayDataObject = {};
+            arrayDataObject[arrayName] = prevState.formData[arrayName];
+            arrayDataObject[arrayName].push(newDataObject);
+            return {
+                ...prevState,
+                formData: {
+                    ...prevState.formData,
+                    ...arrayDataObject
+                }
+            }
+        });
+    };
+
+    changeInput = (arrayName, uniqueId, nameProperty, value, isValues) => {
+        this.setState((prevState) => {
+            let newArray = arrayName === 'initialFarmCount' ? uniqueId : prevState.formData[arrayName].map(item=>{
+                if (item.uniqueId === uniqueId) {
+                    let inputDataObject = {};
+                    if (isValues) {
+                        inputDataObject.valueInput = item.valueInput;
+                        inputDataObject.valueInput[nameProperty] = value;
+                    } else {
+                        inputDataObject[nameProperty] = value;
+                    }
+                    return {
+                        ...item,
+                        ...inputDataObject
+                    }
+                } else {
+                    return item;
+                }
+            });
+
+            let arrayDataObject = {};
+            arrayDataObject[arrayName] = newArray;
+            return {
+                ...prevState,
+                formData : {
+                    ...prevState.formData,
+                    ...arrayDataObject
+                }
+            }
+        })
+
+    };
+
+    deleteInput = (arrayName, uniqueId) => {
+        this.setState((prevState) => {
+            let arrayDataObject = {};
+            arrayDataObject[arrayName] = prevState.formData[arrayName].filter(item=>item.uniqueId !== uniqueId);
+            return {
+                ...prevState,
+                formData : {
+                    ...prevState.formData,
+                    ...arrayDataObject
+                }
+            }
+        })
+    };
+
+
+    renderCommonInputs = (arrayName, data) => {
+        return data.map(item => (
+            <div key={item.uniqueId} className="editable-list-input-container">
+                <input
+                    type="text"
+                    value={item.nameInput}
+                    onChange={(e) => this.changeInput(arrayName, item.uniqueId, 'nameInput', e.target.value, false)}
+                />
+                <input
+                    type="text"
+                    value={item.valueInput}
+                    onChange={(e) => this.changeInput(arrayName, item.uniqueId, 'valueInput', e.target.value, false)}
+                />
+                <button type="reset" onClick={() => this.deleteInput(arrayName, item.uniqueId)}>delete prop</button>
+            </div>
+        ));
+    };
+
+    renderPositionsInputs = (arrayName, data) => {
+        return data.map(item => (
+            <div key={item.uniqueId} className="editable-list-input-container">
+                <label>Name</label>
+                <input
+                    type="text"
+                    value={item.nameInput}
+                    onChange={(e) => this.changeInput(arrayName, item.uniqueId, 'nameInput', e.target.value, false)}
+                />
+                <label>X</label>
+                <input
+                    type="text"
+                    value={item.valueInput.x}
+                    onChange={(e) => this.changeInput(arrayName, item.uniqueId, 'x', e.target.value, true)}
+                />
+                <label>Y</label>
+                <input
+                    type="text"
+                    value={item.valueInput.y}
+                    onChange={(e) => this.changeInput(arrayName, item.uniqueId, 'y', e.target.value, true)}
+                />
+                <button type="reset" onClick={() => this.deleteInput(arrayName, item.uniqueId)}>delete prop</button>
+            </div>
+        ));
+    };
+
     render() {
         return (
             <div className="container">
-                <h2 className="col-50">{this.state.entity}</h2>
-                <AddButton getData={this.getData} />
                 {
-                    false // this.state.isLoaded
-                        ?
-                        'Working in progress'
-                        :
-                        <Loading />
+                    this.state.isLoaded
+                    ?
+                    <form >
+                        <div className="form-container">
+
+                            {/*initialFarmCount*/}
+                            <div className="form-input-container">
+                                <label>initial Farm Count:</label>
+                                <input
+                                    type="text"
+                                    value={this.state.formData.initialFarmCount}
+                                    onChange={(e)=>this.changeInput('initialFarmCount', e.target.value)}
+                                />
+                            </div>
+                            <hr/>
+
+                            {/*farmIndexAndOutputs*/}
+                            <div>
+                                <h2>farm Index And Outputs</h2>
+                                { this.renderCommonInputs('farmIndexAndOutputs', this.state.formData.farmIndexAndOutputs) }
+                                <div>
+
+                                </div>
+                                <button type="reset"
+                                        onClick={()=>this.addInput('farmIndexAndOutputs', false)}
+                                        className="btn btn-save">Add
+                                </button>
+                            </div>
+                            <hr/>
+
+                            {/*farmIndexAndCosts*/}
+                            <div>
+                                <h2>farm Index And Costs</h2>
+                                { this.renderCommonInputs('farmIndexAndCosts', this.state.formData.farmIndexAndCosts) }
+                                <div>
+
+                                </div>
+                                <button type="reset"
+                                        onClick={()=>this.addInput('farmIndexAndCosts', false)}
+                                        className="btn btn-save">Add
+                                </button>
+                            </div>
+                            <hr/>
+
+                            {/*farmIndexAndPositions*/}
+                            <div>
+                                <h2>farm Index And Positions</h2>
+                                { this.renderPositionsInputs('farmIndexAndPositions', this.state.formData.farmIndexAndPositions) }
+                                <div>
+
+                                </div>
+                                <button type="reset"
+                                        onClick={()=>this.addInput('farmIndexAndPositions', true)}
+                                        className="btn btn-save">Add
+                                </button>
+                            </div>
+                            <hr/>
+
+                            {/*form buttons*/}
+                            <div className="modal-footer">
+                                <button type="reset"
+                                        onClick={()=>this.saveData()}
+                                        className="btn btn-save">Save
+                                </button>
+                                <button type="reset"
+                                        className="btn btn-close"
+                                        onClick={()=>this.resetData()}>Reset
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    :
+                    <Loading />
                 }
             </div>
         )
